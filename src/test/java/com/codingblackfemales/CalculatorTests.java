33f435208f73e593f545d307405b61bdd64465ea
AdelabuAderonke <adelabu.aderonke9638@gmail.com>
2023-10-13 09:15:02 +0100

    Added test for multiply and division

--- src/test/java/com/codingblackfemales/CalculatorTests.java
+++ src/test/java/com/codingblackfemales/CalculatorTests.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.IOException;
 
 @DisplayName(value = "The Calculator should")
 public class CalculatorTests {
@@ -23,4 +26,27 @@ public class CalculatorTests {
 
         assertEquals(-2, difference);
     }
+    @Test
+    
+    @DisplayName("Divide two numbers")
+    public void testDivide() {
+        final Integer division = Calculator.divide(16, 4);       
+        assertEquals(4, division);
+        
+    }
+    @Test
+    public void testDivideZero() {
+        assertThrows(ArithmeticException.class, () -> {
+            Calculator.divide(16, 0);
+        });
+    }
+    
+    @Test
+    @DisplayName("Multiply two numbers")
+    public void testMultiply() {
+        final Integer multiply = Calculator.multiply(6, 3);
+        assertEquals(18, multiply);
+        
+    }
+    
 }
